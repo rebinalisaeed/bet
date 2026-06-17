b5b375003180908a4fc85f468539a37a7aa96c35
rebinalisaeed <rebiinaliisaeed@gmail.com>
2026-06-17 15:07:44 +0300

    Delete js directory

--- js/exchange-1.js
+++
@@ -1,4 +0,0 @@
-function calcExchange(){
-        const v = Number(document.getElementById('coinAmount').value || 0);
-        document.getElementById('diamondResult').textContent = '💎 ' + Math.floor(v / 100);
-      }
--- js/index-1.js
+++
@@ -1,3 +0,0 @@
-setTimeout(() => {
-      window.location.href = "login.html";
-    }, 6000);
